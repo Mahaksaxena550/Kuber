70d9a29e75e71a21b43106a17604f4377dd7844c
Mahak Saxena <ms2722528@gmail.com>
2026-04-14 13:17:42 +0530

    Update apps file

--- Backend/apps/authentication/apps.py
+++ Backend/apps/authentication/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
-
 class AuthenticationConfig(AppConfig):
-    name = 'authentication'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.authentication"
+    verbose_name = "Authentication"

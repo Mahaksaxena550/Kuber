cefa9d1cfff4e1312938469f938eda5fa0aacc1f
Mahak Saxena <ms2722528@gmail.com>
2026-04-14 15:20:56 +0530

    Update apps file to correct path

--- Backend/apps/ai_bot/apps.py
+++ Backend/apps/ai_bot/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
-
 class AiBotConfig(AppConfig):
-    name = 'ai_bot'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.ai_bot"
+    verbose_name = "AI Bot"
--- Backend/apps/wallet/apps.py
+++ Backend/apps/wallet/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
-
 class WalletConfig(AppConfig):
-    name = 'wallet'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.wallet"
+    verbose_name = "Wallet"
